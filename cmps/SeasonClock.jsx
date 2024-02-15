a03f674a0896dbca0022233229fd1fe28b06ec9d
Valery <verelly.v@gmail.com>
2024-02-15 15:48:52 +0200

    running clock added

--- cmps/SeasonClock.jsx
+++ cmps/SeasonClock.jsx
@@ -1,17 +1,26 @@
-//import React, { useState } from 'react';
-const { useState } = React;
+const { useState, useEffect } = React;
 
 export function SeasonClock(props) {
   const { seasonClockData } = props;
   const [isDark, setIsDark] = useState(false);
-  //const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date());
 
   const handleClick = () => {
     setIsDark(!isDark);
     //console.log("isToggled:" + isDark);
   };
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId); // Clear the interval when the component unmounts
+  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
+
+
   return (
+
     <section
       className={`season-clock ${isDark ? "toggled" : ""}`}
       onClick={handleClick}
@@ -21,6 +30,7 @@ export function SeasonClock(props) {
       </h1>
       <img src="" alt="season" />
       <h3>{seasonClockData.day}</h3>
+      <p>{date.toLocaleTimeString()}</p> {/* Display the running clock */}
     </section>
   );
 }
