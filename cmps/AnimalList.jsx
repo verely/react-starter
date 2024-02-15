79ea7ee91c01311d8cbf5914920a4275b4c4edc9
Valery <verelly.v@gmail.com>
2024-02-15 11:21:53 +0200

    AnimalList component style added

--- cmps/AnimalList.jsx
+++ cmps/AnimalList.jsx
@@ -5,20 +5,23 @@ export function AnimalList(props) {
   const searchQuery = "https://www.google.com/search?q=";
   return (
     <section className="animal-list">
-      <div>Rare Animals</div>
-      <table>
-        <tbody>
-            {animalData.map((animal, index) => (
-            <tr key={index}>
-                <td>{animal.type}</td>
-                <td>{animal.count}</td>
-                <td>
-                    <a href={searchQuery + animal.type}>search</a>
-                </td>
-            </tr>
-            ))}
-        </tbody>
-      </table>
+      <div className="animal-list-container">
+        <h1>Rare Animals</h1>
+        <table className="animal-table">
+          <tbody>
+              {animalData.map((animal, index) => (
+              <tr key={index}>
+                  <td>{animal.type}</td>
+                  <td>{animal.count}</td>
+                  <td>
+                      <a href={searchQuery + animal.type}>search</a>
+                  </td>
+              </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+
     </section>
   );
 }
