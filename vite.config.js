847da8d8fff55ddf508522d53d56ac44c7f0986b
Valery <verelly.v@gmail.com>
2024-02-21 11:04:52 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/verely/react-starter/',
+  base: '/react-starter/',
 });
